41247ed549857078b8ce16bdf6c6d7f5b794cf33
agus-darmawan <5024211070@mhs.its.ac.is>
2022-05-18 02:52:56 +0700

    update pointer number and add array solution

---
+++ Introduction/8.array-introduction.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#define MAX 10000
+using namespace std;
+
+int main() 
+{ 
+    int a[MAX], N,i;
+    cin>>N;
+    for(i=1;i<=N;i++)
+    {
+        cin>>a[i];
+    }
+    for(i=0;i<N;i++)
+    {
+        cout<<a[N-i]<<" ";
+    } 
+    return 0;
+}
