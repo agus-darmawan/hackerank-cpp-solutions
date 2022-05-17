300b342bd0633c34d13fc5cd01963bd296694b14
agus-darmawan <5024211070@mhs.its.ac.is>
2022-05-18 02:38:23 +0700

    add function solution

---
+++ Introduction/6.function.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+
+int max_of_four(int a, int b, int c, int d){
+    int gre;
+    if(a>b && a>c && a>d){
+        gre = a;
+    }
+    else if(b>a && b>c && b>d){
+        gre = b;
+    }
+    else if(c>a && c>b && c>d){
+        gre = c;
+    }
+    else if(d>a && d>b && d>c){
+        gre = d;
+    }
+return gre;
+}
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+    
+    return 0;
+}
+
